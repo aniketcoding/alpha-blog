c6f32a49c10b21ef215e8d94986369713496570c
Aniket <aniketcoding@gmail.com>
2018-11-24 16:52:49 +0000

    CRUD actions completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+  
+  before_action :set_article, only: [:edit,:show,:destroy,:update]
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
   end
@@ -6,11 +12,41 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(articles_params)
-    @article.save
-    redirect_to article_show(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def destroy
+    @article.delete
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+  
+  def update
+    if @article.update(articles_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
   
   private
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  
   def articles_params
     params.required(:article).permit(:title, :description)
   end
